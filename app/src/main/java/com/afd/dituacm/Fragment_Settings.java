5da290fac3783dc86118084157aaf4ee146bbf5e
Pratim <maitytrisha360@gmail.com>
2022-04-20 23:22:48 +0530

    Completed layout design for setting fragments

--- app/src/main/java/com/afd/dituacm/Fragment_Settings.java
+++ app/src/main/java/com/afd/dituacm/Fragment_Settings.java
@@ -1,19 +1,30 @@
 package com.afd.dituacm;
-
+import android.media.Image;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import javax.net.ssl.SSLEngineResult;
+
 public class Fragment_Settings extends Fragment {
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings,container,false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+
+
         return view;
     }
 }
