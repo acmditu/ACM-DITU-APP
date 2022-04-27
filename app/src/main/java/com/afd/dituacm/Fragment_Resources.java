631a6db9b05099f54bc63b4296789a7a21b0d0be
Pratim <maitytrisha360@gmail.com>
2022-04-27 23:57:59 +0530

    Added Menu Drawer

--- app/src/main/java/com/afd/dituacm/Fragment_Resources.java
+++ app/src/main/java/com/afd/dituacm/Fragment_Resources.java
@@ -1,19 +1,45 @@
 package com.afd.dituacm;
-
+import android.media.Image;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.navigation.NavigationView;
+
+import javax.net.ssl.SSLEngineResult;
+
 public class Fragment_Resources extends Fragment {
+
+    DrawerLayout drawerLayout;
+    NavigationView navview;
+    Toolbar toolbar;
+
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_resources,container,false);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().hide();
+//
+        drawerLayout=view.findViewById(R.id.drawerlayout);
+        navview=view.findViewById(R.id.navigationview);
+        toolbar=view.findViewById(R.id.toolbar);
+
+        ActionBarDrawerToggle toggle=new ActionBarDrawerToggle(getActivity(),drawerLayout,toolbar,R.string.navigation_open,R.string.navigation_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
         return view;
     }
 }
